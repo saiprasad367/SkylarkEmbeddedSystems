6c4df698f6b625a986711d1d39e6e8de2ed53ca9
SaiPrasad Chindam <144154210+saiprasad367@users.noreply.github.com>
2026-01-29 22:22:53 +0530

    Update vite.config.ts

--- skylark-digital-core-main/vite.config.ts
+++ skylark-digital-core-main/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://skylarkembeddedsystems.onrender.com",
         changeOrigin: true,
         secure: false,
       },
